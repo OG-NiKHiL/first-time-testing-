e0774a41f6b313852ee25cdf9b07a0da1fd23e35
NIKHIL <nikhilkanyal10@gmail.com>
2026-03-24 07:45:12 +0530

    new commit

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ img.addEventListener('dblclick',function(){
 
       setTimeout(function(){
             love.style.transform = 'translate(-50% ,-300% ) scale(1) rotate(60deg)'
-      }, 800);
+      }, 600);
       setTimeout(function(){
             love.style.opacity= 0;
-      }, 1000);
+      }, 800);
        setTimeout(function(){
             love.style.transform = 'translate(-50% ,-50% ) scale(0) rotate(0deg)'
-      }, 1200);
+      }, 900);
 });   
